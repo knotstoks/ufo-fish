02964d52ac405a053710a6b58ca30aa178b0a0a5
knotstoks <70320812+knotstoks@users.noreply.github.com>
2021-10-15 19:57:19 +0800

    Add Main Menu and Game Manager Prefab

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,7 +1,41 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
-    
+    [SerializeField] private Image blackScreen;
+    public static MainMenu instance;
+    private void Start() {
+        if (!instance) {
+            instance = this;
+        } else {
+            Destroy(gameObject);
+            return;
+        }
+    }
+    public void NextSceneButton(string sceneName) {
+        StartCoroutine(NextScene(sceneName));
+    }
+    public IEnumerator NextScene(string sceneName) {
+        // Animation for the fade screen
+        StartCoroutine(FadeToBlack());
+        yield return new WaitForSeconds(1);
+        // Go to next scene
+        SceneManager.LoadScene(sceneName);
+        // Fade up to the scene
+        StartCoroutine(FadeOut());
+    }
+    public IEnumerator FadeToBlack() {
+        for (double i = 0; i < 1.04; i += 0.04) {
+            blackScreen.color = new Color(0, 0, 0, (float) i);
+            yield return new WaitForSeconds(0.03f);
+        }
+    }
+    public IEnumerator FadeOut() {
+        for (double i = 0; i < 1.04; i += 0.04) {
+            blackScreen.color = new Color(0, 0, 0,  (float) (1 - i));
+            yield return new WaitForSeconds(0.04f);
+        }
+    }
 }
