7970bf4c1cb78829c54cd51bc6077352483a13dc
redpelican2108 <70840956+redpelican2108@users.noreply.github.com>
2021-10-16 20:25:37 +0800

    fixed hazard collision

--- Assets/Scripts/Hazards/Hazard.cs
+++ Assets/Scripts/Hazards/Hazard.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 
 public abstract class Hazard : MonoBehaviour {
     public float damage;
-    private void OnCollisionEnter2D(Collision2D other) {
+    public float force;
+  
+    private void OnCollisionStay2D(Collision2D other) {
         if (other.gameObject.CompareTag("Player")) {
             Vector2 playerPos = other.gameObject.transform.position;
-            Vector2 pushForce = ((Vector2) transform.position - playerPos).normalized;
-
-            other.gameObject.GetComponent<Rigidbody2D>().AddForce(pushForce);
+            float x = playerPos.x - transform.position.x;
+            float y = Mathf.Abs(playerPos.y - transform.position.y);
+            Vector2 pushForce = new Vector2(x, -1 * y).normalized;
+          
+            other.gameObject.GetComponent<Rigidbody2D>().AddForce(pushForce * force);
         }
     }
 }
